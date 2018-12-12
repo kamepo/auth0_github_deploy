3dd38849a176a6c98bf7b5906093d6b947e66a48
Karen <37811677+kamepo@users.noreply.github.com>
2018-12-12 12:21:43 -0500

    Update set-country.js

--- rules/set-country.js
+++ rules/set-country.js
@@ -2,6 +2,6 @@ function (user, context, callback) {
 	if (context.request.geoip) {
 		user.country = context.request.geoip.country_name;
 	}
-	console.log("geoip");
+	//console.log("geoip");
 	callback(null, user, context);
 }
